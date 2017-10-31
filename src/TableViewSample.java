19af020a36d22bf0e83db9e64f0db869f0a1c04e
romanmarczewski <dharma@codemandala.com>
2017-10-31 14:30:18 +0100

    TableView - add & delete functionality

--- src/TableViewSample.java
+++ src/TableViewSample.java
@@ -1,16 +1,21 @@
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TableViewSample extends Application{
 
     TableView<Product> table;
+    TextField nameInput, priceInput, quantityInput;
 
     public static void main(String[] args) {
         launch(args);
@@ -34,20 +39,62 @@ public class TableViewSample extends Application{
         quantityColumn.setMaxWidth(120);
         quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
 
+        // name, price & quantity input
+        nameInput = new TextField();
+        nameInput.setPromptText("Name");
+        nameInput.setMinWidth(100);
+
+        priceInput = new TextField();
+        priceInput.setPromptText("Price");
+        priceInput.setMinWidth(100);
+
+        quantityInput = new TextField();
+        quantityInput.setPromptText("Quantity");
+        quantityInput.setMinWidth(100);
+
+        // here goes buttons
+        Button addButton = new Button("Add");
+        addButton.setOnAction(e -> addButtonClicked());
+        Button deleteButton = new Button("Delete");
+        deleteButton.setOnAction(e -> deleteButtonClicked());
+
+        HBox manipulateBox = new HBox();
+        manipulateBox.setPadding(new Insets(10));
+        manipulateBox.setSpacing(10);
+        manipulateBox.getChildren().addAll(nameInput, priceInput, quantityInput, addButton, deleteButton);
+
         table = new TableView<>();
         table.setItems(getProduct());
         table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
 
-
-
         VBox vBox = new VBox(10);
-        vBox.getChildren().addAll(table);
+        vBox.getChildren().addAll(table, manipulateBox);
 
 
         Scene scene = new Scene(vBox, 640, 480);
 
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void addButtonClicked() {
+        Product product = new Product();
+        product.setName(nameInput.getText());
+        product.setPrice(Double.parseDouble(priceInput.getText()));
+        product.setQuantity(Integer.parseInt((quantityInput.getText())));
+        table.getItems().add(product);
+        nameInput.clear();
+        priceInput.clear();
+        quantityInput.clear();
+    }
+
+    private void deleteButtonClicked(){
+        ObservableList<Product> productSelected, allProducts;
+        allProducts = table.getItems();
+        productSelected = table.getSelectionModel().getSelectedItems();
+
+        productSelected.forEach(allProducts::remove);
+
     }
 
     //Get all of the products - usually connected to database or file
