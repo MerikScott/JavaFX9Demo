38e27d8cc49bd0caf0c1ba1902a4dc853300bc0e
romanmarczewski <dharma@codemandala.com>
2017-10-31 16:23:14 +0100

    Menu Maker - handling menu clicks

--- src/MenuMaker.java
+++ src/MenuMaker.java
@@ -3,6 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -20,14 +21,32 @@ public class MenuMaker extends Application{
 
         //File menu
         Menu fileMenu = new Menu("File");
+
+        MenuItem newFile = new MenuItem("New...");
+        newFile.setOnAction(e -> System.out.println("Create new file..."));
         fileMenu.getItems().add(new MenuItem("New..."));
+
         fileMenu.getItems().add(new MenuItem("Open..."));
+        fileMenu.getItems().add(new SeparatorMenuItem());
         fileMenu.getItems().add(new MenuItem("Save..."));
         fileMenu.getItems().add(new MenuItem("Settings..."));
+        fileMenu.getItems().add(new SeparatorMenuItem());
         fileMenu.getItems().add(new MenuItem("Exit"));
 
+        //Edit menu
+        Menu editMenu = new Menu("_Edit"); // _should indicate shortcut
+        editMenu.getItems().add(new MenuItem("Cut"));
+        editMenu.getItems().add(new MenuItem("Copy"));
+
+        MenuItem paste = new MenuItem("Paste");
+        paste.setOnAction(e -> System.out.println("Pasting something"));
+        editMenu.getItems().add(paste);
+        paste.setDisable(true);
+
+
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(fileMenu);
+        menuBar.getMenus().addAll(fileMenu, editMenu);
+
 
         layout = new BorderPane();
         layout.setTop(menuBar);
